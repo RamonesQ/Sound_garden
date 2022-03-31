fef08ad0cb7e62753a06c3586ab41e7a32db2c5f
RamonoesQ <ramonqueirozs@gmail.com>
2022-03-30 22:31:07 -0300

    att edit event

---
+++ js/ver-reserva.js
@@ -0,0 +1,30 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const mostrarReservas = async() => { 
+    const lista = document.querySelector('tbody');
+const Options = {
+    method :'GET',
+    redirect :'follow',
+};
+    const request = await fetch (`${BASE_URL}/bookings`, Options)
+    const conteudoResposta = await request.json();
+
+    let Admin = '';
+
+    for(let index = 0; index < conteudoResposta.length; index++){
+        Admin += `<tr>
+        <th scope="row">${index}</th>
+        <td>${conteudoResposta[index].owner_name}</td>
+        <td>${conteudoResposta[index].owner_email}</td>
+        <td>${conteudoResposta[index].number_tickets}</td>
+        <td>
+            <a href="reservas.html?id=${conteudoResposta[index]._id}" class="btn btn-dark">ver reservas</a>
+            <a href="editar-evento.html?id=${conteudoResposta[index]._id}" class="btn btn-secondary">editar</a>
+            <a href="excluir-evento.html?id=${conteudoResposta[index]._id}" class="btn btn-danger">excluir</a>
+        </td>
+    </tr>`
+    }
+    lista.innerHTML = Admin
+};    
+
+mostrarReservas()
