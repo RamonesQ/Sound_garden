dd27f298413b21aea2805f2a017b13b2c5e8a2d1
RamonoesQ <ramonqueirozs@gmail.com>
2022-03-30 23:26:25 -0300

    Edição
    Remoção de codigos antigos comentados

--- js/editar-evento.js
+++ js/editar-evento.js
@@ -14,13 +14,7 @@ const inputLotacao = document.querySelector("#lotacao");
 
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-const form = document.querySelector("form"); // passar para evento onclick, quando acionado o botão enviar
-
-// function pegaId(){
-//     const url = new URL(window.location.href)
-//     const id = url.searchParams.get('_id')
-//     return id
-// }
+const form = document.querySelector("form");
 
 const parametrosURL = new URLSearchParams(window.location.search);
 const parametrosID = parametrosURL.get("id");
@@ -28,8 +22,7 @@ const parametrosID = parametrosURL.get("id");
 const mostrarEvento = async () => {
     const resposta = await fetch(`${BASE_URL}/events/${parametrosID}`, {method: "GET"});
     const respostaAPI = await resposta.json();    
-    
-    
+     
 
 editarNome.value = respostaAPI.name;
 editarAtracoes.value = respostaAPI.attractions;
